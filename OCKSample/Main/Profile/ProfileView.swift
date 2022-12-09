1ef2dd31faf3f8c59e0e4b7f63fc71e2f2d03b3d
LukeTaylor1 <79071882+LukeTaylor1@users.noreply.github.com>
2022-12-08 20:30:43 -0500

    Profile and contact enhancements (#6)
    * on #2 part 3 of the assignment
    
    * barely anything

--- OCKSample/Main/Profile/ProfileView.swift
+++ OCKSample/Main/Profile/ProfileView.swift
@@ -5,7 +5,6 @@
 //  Created by Corey Baker on 11/24/20.
 //  Copyright © 2020 Network Reconnaissance Lab. All rights reserved.
 //
-
 import SwiftUI
 import CareKitUI
 import CareKitStore
@@ -13,139 +12,105 @@ import CareKit
 import os.log
 
 struct ProfileView: View {
+    @Environment(\.tintColor) private var tintColor
     @StateObject var viewModel = ProfileViewModel()
     @ObservedObject var loginViewModel: LoginViewModel
-    @State var firstName = ""
-    @State var lastName = ""
-    @State var birthday = Date()
-    
-    
-    @State var selectedTask = "CareKit Task"
-    let tasks = ["CareKit Task",  "HealthKit Task"]
-    @State var showingAdvanceOptions = false
-    @State var CareKitTaskTitle = ""
-    @State var CareKitTaskInstructions = ""
-    @State var HealthKitTaskTitle = ""
-    @State var HealthKitTaskInstuctions = ""
-    @State var isPresentingAddTask = false
-    
 
     var body: some View {
-        VStack {
-            VStack(alignment: .leading) {
-                NavigationView {
-                    Text("Hello")
-                    .toolbar {
-                            ToolbarItem(placement: .navigationBarLeading){
-                                Button("Add Task"){
-                                    Task {
-                                        self.isPresentingAddTask.toggle()
-                                    }
-                                }
-                                Label("Add Task", systemImage: "plus")
+        NavigationView {
+            VStack {
+                VStack {
+                    ProfileImageView(viewModel: viewModel)
+                    Form {
+                        Section(header: Text("About")) {
+                            TextField("First Name", text: $viewModel.firstName)
+                            TextField("Last Name", text: $viewModel.lastName)
+                            DatePicker("Birthday",
+                                       selection: $viewModel.birthday,
+                                       displayedComponents: [DatePickerComponents.date])
+                            Picker(selection: $viewModel.sex,
+                                   label: Text("Sex")) {
+                                Text(OCKBiologicalSex.female.rawValue).tag(OCKBiologicalSex.female)
+                                Text(OCKBiologicalSex.male.rawValue).tag(OCKBiologicalSex.male)
+                                Text(viewModel.sex.rawValue)
+                                    .tag(OCKBiologicalSex.other(viewModel.sexOtherField))
                             }
+                            TextField("Allergies", text: $viewModel.allergies as! String)
                         }
-                    .sheet(isPresented: $isPresentingAddTask) {
+                        Section(header: Text("Contact")) {
+                            TextField("Street", text: $viewModel.street)
+                            TextField("City", text: $viewModel.city)
+                            TextField("State", text: $viewModel.state)
+                            TextField("Postal code", text: $viewModel.zipcode)
+                            
+                            TextField("Email Address", text: $viewModel.emailAddress)
+                            TextField("Messaging Numbers", text: $viewModel.messagingNumbers)
+                            TextField("Phone Number", text: $viewModel.phoneNumber)
+                            TextField("Other Contact Info", $viewModel.otherContactInfo)
+                            
+                        }
+                    }
+                }
+
+                Button(action: {
+                    Task {
+                        await viewModel.saveProfile()
+                    }
+                }, label: {
+                    Text("Save Profile")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                })
+                .background(Color(.green))
+                .cornerRadius(15)
+
+                // Notice that "action" is a closure (which is essentially
+                // a function as argument like we discussed in class)
+                Button(action: {
+                    Task {
+                        await loginViewModel.logout()
+                    }
+                }, label: {
+                    Text("Log Out")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                })
+                .background(Color(.red))
+                .cornerRadius(15)
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button("My Contact") {
+                        viewModel.isPresentingContact = true
+                    }
+                    .sheet(isPresented: $viewModel.isPresentingContact) {
+                        MyContactView()
+                    }
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Add Task") {
+                        viewModel.isPresentingAddTask = true
+                    }
+                    .sheet(isPresented: $viewModel.isPresentingAddTask) {
                         TaskView()
                     }
                 }
-                /*
-                // Added lines below from: https://www.hackingwithswift.com/quick-start/swiftui/pickers-in-forms
-                NavigationView{
-                    Form{
-                        Picker("Task", selection: $selectedTask){
-                            ForEach(tasks, id: \.self){
-                                Text($0)
-                            }
-                        }
-                        .pickerStyle(.automatic)
-                    }
-                } .navigationTitle("Select Type of task")
-                // End of Wrapping
-                Form{
-                    switch selectedTask {
-                    case "CareKit Task":
-                        TextField("CT Title", text: $CareKitTaskTitle)
-                        TextField("Task Instuction", text: $CareKitTaskInstructions)
-                    case "HealthKit Task":
-                        TextField("hK Title", text:$HealthKitTaskTitle)
-                        TextField("Task Instructions", text: $HealthKitTaskInstuctions)
-                    default:
-                        TextField("CT Title", text: $CareKitTaskTitle)
-                        TextField("Task Instructions", text: $CareKitTaskInstructions)
-                    }
-                }
-                 */
-                TextField("First Name", text: $firstName)
-                    .padding()
-                    .cornerRadius(20.0)
-                    .shadow(radius: 10.0, x: 20, y: 10)
-
-                TextField("Last Name", text: $lastName)
-                    .padding()
-                    .cornerRadius(20.0)
-                    .shadow(radius: 10.0, x: 20, y: 10)
-
-                DatePicker("Birthday", selection: $birthday, displayedComponents: [DatePickerComponents.date])
-                    .padding()
-                    .cornerRadius(20.0)
-                    .shadow(radius: 10.0, x: 20, y: 10)
-            } .toolbar {
-                ToolbarItem(placement: .navigationBarLeading){
-                    Label("Add Task", systemImage: "plus")
-                }
             }
-
-
-            Button(action: {
-                Task {
-                    do {
-                        try await viewModel.saveProfile(firstName,
-                                                        last: lastName,
-                                                        birth: birthday)
-                    } catch {
-                        Logger.profile.error("Error saving profile: \(error.localizedDescription)")
-                    }
-                }
-            }, label: {
-                Text("Save Profile")
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .padding()
-                    .frame(width: 300, height: 50)
-            })
-            .background(Color(.green))
-            .cornerRadius(15)
-
-            // Notice that "action" is a closure (which is essentially
-            // a function as argument like we discussed in class)
-            Button(action: {
-                Task {
-                    await loginViewModel.logout()
-                }
-            }, label: {
-                Text("Log Out")
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .padding()
-                    .frame(width: 300, height: 50)
-            })
-            .background(Color(.red))
-            .cornerRadius(15)
+            .sheet(isPresented: $viewModel.isPresentingImagePicker) {
+                ImagePicker(image: $viewModel.profileUIImage)
+            }
+            .alert(isPresented: $viewModel.isShowingSaveAlert) {
+                return Alert(title: Text("Update"),
+                             message: Text(viewModel.alertMessage),
+                             dismissButton: .default(Text("Ok"), action: {
+                                viewModel.isShowingSaveAlert = false
+                             }))
+            }
         }
-        
-        
-        .onReceive(viewModel.$patient, perform: { patient in
-            if let currentFirstName = patient?.name.givenName {
-                firstName = currentFirstName
-            }
-            if let currentLastName = patient?.name.familyName {
-                lastName = currentLastName
-            }
-            if let currentBirthday = patient?.birthday {
-                birthday = currentBirthday
-            }
-        })
     }
 }
 
